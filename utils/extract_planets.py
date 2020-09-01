66595beef3f88439b01c4a6b3fc0a52a9054b914
Sebastian Bialy <66225160+sebobialy@users.noreply.github.com>
2020-09-01 18:19:47 +0200

    more items

--- utils/extract_planets.py
+++ utils/extract_planets.py
@@ -17,8 +17,21 @@ class Item( Enum ):
 	Exit = 9
 	Box = 10
 	Dust = 11
+	OneUp = 12
+	WallAlternate = 13
 	
-	Teleporter1 = 100
+	MagnetLeft = 20
+	MagnetRight = 21
+	
+	Teleporter0 = 100
+	Teleporter1 = 101
+	Teleporter2 = 102
+	Teleporter3 = 103
+	Teleporter4 = 104
+	Teleporter5 = 105
+	Teleporter6 = 106
+	Teleporter7 = 107
+	Teleporter8 = 108
 	
 	LaserRight = 200
 	LaserDown = 201
@@ -29,6 +42,7 @@ class Item( Enum ):
 	CannonDown = 301
 	CannonLeft = 302
 	CannonUp = 303
+	RotatingCannon = 304
 	
 	OwlRight = 400
 	OwlDown = 401
@@ -40,11 +54,19 @@ class Item( Enum ):
 	CreatureLeft = 412
 	CreatureUp = 413
 	
+	BirdLeft = 420
+	BirdRight = 421
+	BirdDown = 422
+	BirdUp = 423
+	
+	Eyes = 500
+	
 	Void = 1000
 
 atariToItem = {
 	  ord(' ') : Item.Empty
 	, 0xa0 : Item.Wall
+	, 0x5c : Item.WallAlternate
 
 	, ord('#') : Item.Box
 	, ord('$') : Item.Screw
@@ -56,14 +78,28 @@ atariToItem = {
 	, ord('=') : Item.Key
 	, ord('?') : Item.Suprise
 	, 0x14 : Item.Exit
+	, ord('+') : Item.OneUp
+	, 0x13 : Item.Void
+	, ord('(') : Item.MagnetRight
+	, ord(')') : Item.MagnetLeft
 
+	, ord('0') : Item.Teleporter0
 	, ord('1') : Item.Teleporter1
+	, ord('2') : Item.Teleporter2
+	, ord('3') : Item.Teleporter3
+	, ord('4') : Item.Teleporter4
+	, ord('5') : Item.Teleporter5
+	, ord('6') : Item.Teleporter6
+	, ord('7') : Item.Teleporter7
+	, ord('8') : Item.Teleporter8
 	
 	, 0x27 : Item.LaserDown
 	, ord('^') : Item.LaserUp
 
 	, ord('>') : Item.CannonRight
+	, 0x1e : Item.CannonLeft
 	, 0x1d : Item.CannonDown
+	, ord('-') : Item.RotatingCannon
 	
 	, ord('A') : Item.OwlRight
 	, ord('B') : Item.OwlDown
@@ -75,12 +111,20 @@ atariToItem = {
 	, ord('G') : Item.CreatureLeft
 	, ord('H') : Item.CreatureUp
 	
+	, ord( 'I') : Item.BirdLeft
+	, ord( 'J') : Item.BirdRight
+	, ord( 'L' ) : Item.BirdDown
+	, ord( 'K' ) : Item.BirdUp
+	
+	, ord('&') : Item.Eyes
+	
 	, ord('*') : Item.Robbo
 }
 
 ItemToDump = {
 	  Item.Empty : ' '
 	, Item.Wall : '\u25A1'
+	, Item.WallAlternate : '\u25A0'
 	, Item.Robbo : '*'
 	, Item.Screw : '$'
 	, Item.Ammo : '!'
@@ -91,10 +135,14 @@ ItemToDump = {
 	, Item.Key : '='
 	, Item.Exit : '\u263C'
 	, Item.Suprise : '?'
+	, Item.OneUp : '+'
+	, Item.MagnetLeft : ')'
+	, Item.MagnetRight : '('
 	, Item.CannonRight : '\u25B7'
 	, Item.CannonDown : '\u25BD'
 	, Item.CannonLeft : '\u25C1'
 	, Item.CannonUp : '\u25B3'
+	, Item.RotatingCannon : '\u25C7'
 	, Item.LaserRight : '\u25B6'
 	, Item.LaserDown : '\u25BC'
 	, Item.LaserLeft : '\u25C0'
@@ -107,7 +155,20 @@ ItemToDump = {
 	, Item.CreatureDown : 'F'
 	, Item.CreatureLeft : 'G'
 	, Item.CreatureUp : 'H'
+	, Item.BirdLeft : 'I'
+	, Item.BirdRight : 'J'
+	, Item.BirdDown : 'L'
+	, Item.BirdUp : 'K'
+	, Item.Eyes : '&'
+	, Item.Teleporter0 : '0'
 	, Item.Teleporter1 : '1'
+	, Item.Teleporter2 : '2'
+	, Item.Teleporter3 : '3'
+	, Item.Teleporter4 : '4'
+	, Item.Teleporter5 : '5'
+	, Item.Teleporter6 : '6'
+	, Item.Teleporter7 : '7'
+	, Item.Teleporter8 : '8'
 
 
 	, Item.Void : '.'
@@ -149,4 +210,4 @@ if __name__ == "__main__":
 	if len( sys.argv ) != 2:
 		raise RuntimeError( "Expected a file name" )
 
-dumpPlanet( getPlanets( atari_file.ExecutableFile( sys.argv[1] ) )[4] )
+dumpPlanet( getPlanets( atari_file.ExecutableFile( sys.argv[1] ) )[10] )
